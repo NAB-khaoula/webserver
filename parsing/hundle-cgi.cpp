49c9092708c6438848d1ec0e68514674bf131faf
mojahid-belaman <mojahidbelaman@gmail.com>
2022-02-17 11:53:59 +0100

    add envirenment variable the POST in CGI

--- parsing/hundle-cgi.cpp
+++ parsing/hundle-cgi.cpp
@@ -13,11 +13,31 @@ std::string    runCgi()
     std::string str;
     std::string path_cgi;
 
+    //NOTE - The full path to the CGI script.
     setenv("SCRIPT_FILENAME", "../parsing/tmp/index.py", true);
+    //NOTE - This variable is specific to requests made with HTTP.
     setenv("REQUEST_METHOD", "GET", true);
-    setenv("REDIRECT_STATUS", "0", true);
+    //NOTE - If cgi.force_redirect is turned on, and you are not running under web servers, you may need to set an environment variable name that PHP will look for to know it is OK to continue execution.
+    setenv("REDIRECT_STATUS", "200", true);
+    //NOTE - The version of the CGI specification to which this server complies
     setenv("GATEWAY_INTERFACE", "CGI/1.1", true);
+    //NOTE - A path to be interpreted by the CGI script.
     setenv("PATH_INFO", "/Users/mbelaman/Desktop/webserver/parsing/", true);
+    //NOTE - The length of the query information. It is available only for POST requests.
+    setenv("CONTENT_LENGTH", "", true);
+    //NOTE - The data type of the content. Used when the client is sending attached content to the server. For example, file upload.
+    setenv("CONTENT_TYPE", "", true);
+    //NOTE - The URL-encoded information that is sent with GET method request.
+    setenv("QUERY_STRING", "", true);
+    //NOTE - The server's hostname or IP Address
+    setenv("SERVER_NAME", "", true);
+    //NOTE - The port on which this request was received; appropriate as the port part of a URI.
+    setenv("SERVER_PORT", "", true);
+    //NOTE - The name and revision of the information protocol this request came in with.
+    setenv("SERVER_PROTOCOL", "", true);
+    //NOTE - The name and version of the information server software answering the request (and running the gateway).
+    setenv("SERVER_SOFTWARE", "", true);
+
     // path_cgi = "/Users/mbelaman/goinfre/.brew/bin/php-cgi";
     path_cgi = "/usr/bin/python";
 
