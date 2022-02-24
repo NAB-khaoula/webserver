227858cb69a7908fffaa1a0ee84042f13b75f7f1
mojahid-belaman <mojahidbelaman@gmail.com>
2022-02-24 12:57:59 +0100

    hundle leak in parsing

--- parsing/Location.hpp
+++ parsing/Location.hpp
@@ -33,23 +33,23 @@ class Location
 		std::string                  get_delete_enble();
 		std::string                  get_cgi();
 	
-		void                        set_path(std::string, int &);
+		void                        set_path(std::string, int &, struct ws &);
 		void                        set_match(bool);
-		void                        set_autoindex(std::string, int &);
-		void                        set_index(std::string, int &);
-		void                        set_methods(std::string, int &);
-		void                        set_return(std::string, int  &);
-		void                        set_upload(std::string, int &);
-		void                        set_upload_enble(std::string, int &);
-		void                        set_delete_enble(std::string, int &);
-		void                        set_cgi(std::string, int &);
+		void                        set_autoindex(std::string, int &, struct ws &);
+		void                        set_index(std::string, int &, struct ws &);
+		void                        set_methods(std::string, int &, struct ws &);
+		void                        set_return(std::string, int  &, struct ws &);
+		void                        set_upload(std::string, int &, struct ws &);
+		void                        set_upload_enble(std::string, int &, struct ws &);
+		void                        set_delete_enble(std::string, int &, struct ws &);
+		void                        set_cgi(std::string, int &, struct ws &);
 		void                        clear();
 		~Location();
 };
 
 std::vector<std::string>    ft_splitSpace(std::string str, char c);
-std::vector<std::string>    delete_brace(std::string str, int &);
-void                        errors(int index, int &, std::string);
+std::vector<std::string>    delete_brace(std::string str, int &, struct ws &);
+void                        errors(int index, int &, std::string, struct ws &);
 std::string&                rightTrim(std::string& str);
 std::string&                leftTrim(std::string& str);
 bool                        isNumber(std::string s);
